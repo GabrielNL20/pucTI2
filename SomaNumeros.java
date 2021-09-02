d6130d9e5529706be894bda6316eea5268eecf20
gabrielnlourenço <gabngl@gmail.com>
2021-09-02 18:48:38 -0300

    Add files via upload

---
+++ SomaNumeros.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class SomaNumeros {
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		int x; int y;int soma;
+		System.out.println("digite o primeiro numero");
+		x=entrada.nextInt();
+		System.out.println("digite o segundo numero");
+		y=entrada.nextInt();
+		soma=x+y;
+		System.out.println("o resultado da soma é "+soma);
+
+	}
+
+}//
